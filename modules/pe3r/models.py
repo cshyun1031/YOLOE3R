8168032a25219cf59a6f6c5dd6a8605f1593d47e
cshyun1031 <166009462+cshyun1031@users.noreply.github.com>
2025-11-26 12:56:49 +0900

    Update models.py

--- modules/pe3r/models.py
+++ modules/pe3r/models.py
@@ -1,15 +1,13 @@
 import os
 import sys
-sys.path.append(os.path.abspath('./modules/ultralytics'))
 
 from transformers import AutoTokenizer, AutoModel, AutoProcessor, SamModel
 from modules.mast3r.model import AsymmetricMASt3R
 
 # from modules.sam2.build_sam import build_sam2_video_predictor
-from modules.mobilesamv2.promt_mobilesamv2 import ObjectAwareModel
-from modules.mobilesamv2 import sam_model_registry
 
 from sam2.sam2_video_predictor import SAM2VideoPredictor
+from ultralytics import YOLO
 
 class Models:
     def __init__(self, device):
@@ -26,27 +24,7 @@ class Models:
         # self.sam2.eval()
         self.sam2 = SAM2VideoPredictor.from_pretrained('facebook/sam2.1-hiera-large', device=device)
 
-        # -- mobilesamv2 & sam1 --
-        # SAM1_ENCODER_CKP = './checkpoints/sam_vit_h.pt'
-        # SAM1_ENCODER_CKP = 'facebook/sam-vit-huge/model.safetensors'
-        SAM1_DECODER_CKP = './checkpoints/Prompt_guided_Mask_Decoder.pt'
-        self.mobilesamv2 = sam_model_registry['sam_vit_h'](None)
-        # image_encoder=sam_model_registry['sam_vit_h_encoder'](SAM1_ENCODER_CKP)
-        sam1 = SamModel.from_pretrained('facebook/sam-vit-huge')
-        image_encoder = sam1.vision_encoder
-
-        prompt_encoder, mask_decoder = sam_model_registry['prompt_guided_decoder'](SAM1_DECODER_CKP)
-        self.mobilesamv2.prompt_encoder = prompt_encoder
-        self.mobilesamv2.mask_decoder = mask_decoder
-        self.mobilesamv2.image_encoder=image_encoder
-        self.mobilesamv2.to(device=device)
-        self.mobilesamv2.eval()
-
-        # -- yolov8 --
-        YOLO8_CKP='./checkpoints/ObjectAwareModel.pt'
-        self.yolov8 = ObjectAwareModel(YOLO8_CKP)
-
+        self.seg_model = YOLO("/content/PE3R/yoloe-11l-seg.pt")
+        self.seg_model.set_classes(["bed", "chair", "desk","table",'book','sofa'])
         # -- siglip --
-        self.siglip = AutoModel.from_pretrained("google/siglip-large-patch16-256", device_map=device)
-        self.siglip_tokenizer = AutoTokenizer.from_pretrained("google/siglip-large-patch16-256")
-        self.siglip_processor = AutoProcessor.from_pretrained("google/siglip-large-patch16-256")
+      
